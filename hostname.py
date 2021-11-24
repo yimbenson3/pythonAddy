a5cc394655021c738b2e67090b8fa5f2cc083bf0
barryyim <bbarryyim@gmail.com>
2021-11-23 19:05:05 -0800

    Hello world! Flask webapp

---
+++ hostname.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/hostname")
+def hostname():
+    return socket.gethostname()
+    
+if __name__ == "__main__":
+    app.run(debug=True)
