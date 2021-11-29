d060ea35a6a8232dec5a07489c79b69e9a9ea274
barryyim <bbarryyim@gmail.com>
2021-11-28 16:46:25 -0800

    Moving configs into ConfigMap

--- config/loadConfig.py
+++ config/loadConfig.py
@@ -1,5 +1,6 @@
 import yaml
 import logging
+import os
 
 loggerAzure = logging.getLogger("azure.core.pipeline.policies.http_logging_policy")
 loggerAzure.setLevel(logging.WARNING)
@@ -7,7 +8,8 @@ loggerAzure.setLevel(logging.WARNING)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
 def loadConfigurations():
-    with open("config/mainConfiguration.yml","r") as file:
+    os.chdir('/etc/config')
+    with open("mainConfiguration.yml","r") as file:
         try:
             config = yaml.safe_load(file)
             return config
